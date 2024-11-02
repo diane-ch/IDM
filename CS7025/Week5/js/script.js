b127b91e8f51fc76e3556ad64272ebcc02f4f4ce
Diane Chounlamountry <diane.chounlamountry@gmail.com>
2024-11-02 15:28:39 +0000

    better design for pick a movie

--- CS7025/Week5/js/script.js
+++ CS7025/Week5/js/script.js
@@ -6,11 +6,16 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("pickImageButton").addEventListener('click', () => {
         console.log("click");
 
+        // Pick a random integer between 0 and 3
         let max = 4;
         let randomInt = Math.floor(Math.random() * max);
         console.log("random nb", randomInt);
-        let img_value= "../img/"+ images[randomInt];
+
+        // Pick an image using the random number
+        let img_value= "img/"+ images[randomInt];
         console.log("img", img_value);
+
+        // Setting the source of the image with the ID #moviePoster 
         document.getElementById("moviePoster").setAttribute("src", img_value)
 
     });
