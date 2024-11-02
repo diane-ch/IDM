29299331a216d978a92ef5643e098e254ffdd429
Diane Chounlamountry <diane.chounlamountry@gmail.com>
2024-11-02 14:46:20 +0000

    DOM manipulation

--- CS7025/Week4/js/script.js
+++ CS7025/Week4/js/script.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+    console.log("ready");
 
-    console.log("ready")
+    // Creating a paragraph and adding it to the html page
+    let paragraph = document.createElement("p");
+    paragraph.textContent = "Hello World!";    
+    document.body.appendChild(paragraph);
+
+    // Creating a button that says "click" and adding it to the html page
+    let btn = document.createElement("button");
+    btn.textContent = "Click";
+    document.body.appendChild(btn);
+
+    // Changing the ID of an element
+    document.getElementById("items").setAttribute("id", "list");
+
 
 })
