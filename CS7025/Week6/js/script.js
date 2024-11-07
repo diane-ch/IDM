087f4fd897801238d7ba034e2dd469ed16f9ae34
Diane Chounlamountry <diane.chounlamountry@gmail.com>
2024-11-07 11:54:11 +0000

    json file

--- CS7025/Week6/js/script.js
+++ CS7025/Week6/js/script.js
@@ -1,33 +1,31 @@
-$(document).ready(function () {
-    let data = [];
+// The use of the JSON file is working if the webpage is open through the www folder
+// https://www.scss.tcd.ie/~chounlad/javascript/Week6/index.html
 
-/*     $.ajax({
+$(document).ready(function () {
+    let cities = [];
+    let currentCityIndex = 0;
+
+    $.ajax({
         url: "images.json",
         dataType: "json",
         success: function (data) {
-            console.log(data);
+            // Store the data from JSON into the cities array
+            cities = data;
 
+            // Button click event to change the background
             $('#changeBg').click(function() {
-                changeBackground($('.wrapper'), 'london.jpg')
-            })
+                const city = cities[currentCityIndex];
+                changeBackground($('.wrapper'), city.img_path);
+                currentCityIndex = (currentCityIndex + 1) % cities.length;
+            });
         },
 
         error: function(){
-            console.log("Error while loading the JSON file")
+            console.log("Error while loading the JSON file");
         }
-    }); */
+    });
 
-    let cities = ['barcelona', 'london', 'new-york', 'paris'];
-    let currentCityIndex = 0;
-
-
-    $('#changeBg').click(function() {
-        changeBackground($('.wrapper'), cities[currentCityIndex]);
-        currentCityIndex = (currentCityIndex + 1) % cities.length;
-    })
-    
-    function changeBackground(element, city){
-        element.css("background-image", `url("images/${city}.jpg")`);
+    function changeBackground(element, imgPath){
+        element.css("background-image", `url("images/${imgPath}")`);
     }
 });
-
