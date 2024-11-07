c72699e089deecf9a740a7f919489759ef64c84c
Diane Chounlamountry <diane.chounlamountry@gmail.com>
2024-11-07 10:03:46 +0000

    lab 1

--- CS7025/LAB1/js/threeinarow.js
+++ CS7025/LAB1/js/threeinarow.js
@@ -29,7 +29,6 @@ $(document).ready(function() {
         cells[i].style.width = cellDim + 'px';
     }
         
-
     // the row the user just picked
     let selectedRow = 0;
 
@@ -40,8 +39,65 @@ $(document).ready(function() {
     // ------------------- FUNCTIONS -------------------
 
     function checkForWin() {
+
+        // Check rows for a win
+        for (let row = 0; row < 3; row++) {
+            if (table[row][0] == table[row][1] && table[row][1] == table[row][2] && table[row][0] != '') {
+                return true; // Row win
+            }
+        }
+
+        // Check columns for a win
+        for (let col = 0; col < 3; col++) {
+            if (table[0][col] == table[1][col] && table[1][col] == table[2][col] && table[0][col] != '') {
+                return true; // Column win
+            }
+        }
+
+        // Check diagonals for a win
+        if ((table[0][0] == table[1][1] && table[1][1] == table[2][2] && table[0][0] != '') ||
+            (table[0][2] == table[1][1] && table[1][1] == table[2][0] && table[0][2] != '')) {
+            return true; // Diagonal win
+        }
+
+        // No win condition found
         return false;
     }
+
+
+    function sendWinNotification() {
+        alert("Player " + playerSymbols[player] + " has won!");
+        reset()
+    }
+
+    function reset() {
+        // reset our javascript
+        table = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ];
+
+        // reset our html
+        let cells = document.querySelectorAll('td')
+        for (var i=0; i<cells.length; i++) {
+            $(cells[i]).text('');
+        }
+    }
+
+    function isFull() {
+        // loop through the table variable
+        // if we find a blank space, return false
+        for (var row=0; row<3; row++){
+            for (var col=0; col<3; col++){
+                if (table[row][col]==""){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 
     function takeTurn() {
         if (isFree(selectedRow, selectedCol)) {
@@ -50,15 +106,31 @@ $(document).ready(function() {
             var desiredTd = $('#table tr:eq(' + selectedRow + ') td:eq(' + selectedCol + ')');
             desiredTd.html(playerSymbols[player]);
             if (checkForWin()) {
-                // end game TO DO
+                setTimeout(function() {
+                    // execute after a small delay to allow HTML to update
+                    sendWinNotification();
+                }, 100);                
+                
             } else {
-                // change player
-                if (player == 0) {
-                    player = 1;
-                } else if (player == 1) {
-                    player = 0;
-                }
+                if (isFull()) {
+                    alert("The table is full ! Nobody won...");
+                    reset();
 
+                } else {
+                    // change player
+                    if (player == 0) {
+                        player = 1;
+                    } else if (player == 1) {
+                        player = 0;
+                    }
+                    // change it in the HTML
+                    currentPlayer = $('#currentPlayer')
+                    currentPlayer.html("Current player : " + playerSymbols[player])
+                    // more efficient syntax : 
+                        // $('#currentPlayer').html("Current player : " + playerSymbols[player]);
+
+                }
+                
 
             }
           
@@ -98,6 +170,10 @@ $(document).ready(function() {
         console.log('Selected cell - row: ', selectedRow, ', col: ', selectedCol);
         
         takeTurn();
+    });
+
+    $('button').on('click', function() {
+        reset();
     });
 
 
