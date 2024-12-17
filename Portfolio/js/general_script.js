b575f79667802b170ae439a0f48ea4a0ecce42d8
diane-ch <diane.chounlamountry@gmail.com>
2024-12-17 13:45:19 +0100

    typewriting if visible

--- Portfolio/js/general_script.js
+++ Portfolio/js/general_script.js
@@ -2,37 +2,18 @@
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    // My name (Diane Chounlamountry) on the homepage
+    // My name on the homepage
     const my_name = document.querySelector("#my-name");
     
     // Checking if the element exists before applying the typewriter effect
     if (my_name) {
-        typewriterEffect(my_name, my_name.textContent, 50);  
+        applyTypewriterEffectIfVisible(my_name, 50); 
     }
 
     // Header "About Me" on the homepage
     const about_me = document.querySelector(".about-me h2");
-
-    // Check if the element exists
     if (about_me) {
-        // An IntersectionObserver triggers the typewriter effect only when the element is visible
-        // Without this, by the time the user scrolls down, the effect has already happened.
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                // Check if the element is visible in the viewport
-                if (entry.isIntersecting) {
-                    typewriterEffect(about_me, about_me.textContent, 120);  
-                    // Once the effect is applied, stop observing the element to prevent it from running again
-                    observer.unobserve(entry.target);
-                }
-            });
-        }, {
-            // 100% of the element must be visible to trigger the effect
-            threshold: 1
-        });
-
-        // Necessary to add the .observe() method. IntersectionObserver does not start without it.
-        observer.observe(about_me);
+        applyTypewriterEffectIfVisible(about_me, 120);
     }
 });
 
@@ -52,4 +33,24 @@ function typewriterEffect(element, text, delay = 100) {
         }
     }
     type();  // Initial call to the type function to start the effect (it's a recursive function)
+}
+
+// An IntersectionObserver triggers the typewriter effect only when the element is visible
+// Without this, by the time the user scrolls down, the effect has already happened.
+function applyTypewriterEffectIfVisible(element, delay) {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            // Check if the element is visible in the viewport
+            if (entry.isIntersecting) {
+                typewriterEffect(element, element.textContent, delay);  
+                // Once the effect is applied, stop observing the element to prevent it from running again
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 1
+    });
+
+    // Necessary to add the .observe() method. IntersectionObserver does not start without it.
+    observer.observe(element);
 }
